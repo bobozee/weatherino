0445572ce3c28face07d1bebae20f842b7775bef
bobozee <monimitev@gmail.com>
2020-05-31 21:05:12 +0200

    increased sleep time

--- src/main.cpp
+++ src/main.cpp
@@ -304,5 +304,5 @@ void loop() {
   digitalWrite(connectionLED, LOW);
   unsigned long timeoutTime = (60 - minute(unixtime)) * 60000;
   Serial.println("All done! Disconnecting and sleeping until the next hour. 'Till then!");
-  delay(timeoutTime);
+  delay(timeoutTime + 10000);
 }
